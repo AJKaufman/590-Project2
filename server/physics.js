7a1e9a313b3b6425b789204074e585eedd000313
AJKaufman <ak3012@rit.edu>
2017-04-06 22:21:29 -0400

    pushing code just in case I don't like what I did

---
+++ server/physics.js
@@ -0,0 +1,13 @@
+let charList = {};
+
+const setCharacterList = (characterList) => {
+  charList = characterList;
+};
+
+const setCharacter = (character) => {
+  charList[character.hash] = character;
+};
+
+
+module.exports.setCharacterList = setCharacterList;
+module.exports.setCharacter = setCharacter;
